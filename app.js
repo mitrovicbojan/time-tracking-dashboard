3dea9d85074ea0ee166286d6614131844bb2ca53
unknown <mitrovicbojan87@gmail.com>
2024-01-15 21:08:31 +0200

    btn

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ async function timeTrackingData() {
   }
   console.log(timeFrames);
 
-  const btnDay = document.querySelector(".btn");
+  const btnDay = document.querySelector(".btnDay");
+  const btnWeek = document.querySelector(".btnWeek");
+  const btnMonth = document.querySelector(".btnMonth");
   const titleDiv = document.querySelectorAll(".subtitle");
   const currDiv = document.querySelectorAll(".curr");
   const prevDiv = document.querySelectorAll(".previous");
@@ -29,6 +31,28 @@ async function timeTrackingData() {
   btnDay.addEventListener("click", function () {
     const curArr = timeFrames.map((current) => current.daily.current);
     const prevArr = timeFrames.map((current) => current.daily.previous);
+
+    currDiv.forEach((item, i) => (item.textContent = `${curArr[i]}hrs`));
+    prevDiv.forEach(
+      (item, i) => (item.textContent = `PREVIOUS - ${prevArr[i]}hrs`)
+    );
+  });
+  btnWeek.addEventListener("click", function () {
+    const curArr = timeFrames.map((current) => current.weekly.current);
+    const prevArr = timeFrames.map((current) => current.weekly.previous);
+
+    currDiv.forEach((item, i) => (item.textContent = `${curArr[i]}hrs`));
+    prevDiv.forEach(
+      (item, i) => (item.textContent = `PREVIOUS - ${prevArr[i]}hrs`)
+    );
+  });
+  btnMonth.addEventListener("click", function (e) {
+    console.log(e.target.textContent);
+    // const clickedBtn = e.target.textContent;
+    // console.log(clickedBtn.toLowerCase());
+
+    const curArr = timeFrames.map((current) => current.monthly.current);
+    const prevArr = timeFrames.map((current) => current.monthly.previous);
 
     currDiv.forEach((item, i) => (item.textContent = `${curArr[i]}hrs`));
     prevDiv.forEach(
