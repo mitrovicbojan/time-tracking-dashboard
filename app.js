4fca45af46e712281b9233a5c08e25b053d0c05b
unknown <mitrovicbojan87@gmail.com>
2024-01-28 20:53:38 +0200

    card

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
-// import data from "/data.json" assert { type: "json" };
-// let subtitle = document.querySelector(".box");
-// let setData = [];
-
-// for (let [i, time] of data.entries(data)) {
-//   setData.push(time);
-// }
-// const titlesArr = setData.map((title) => title.title);
-
 async function timeTrackingData() {
   const response = await fetch("/data.json");
   const data = await response.json();
-  console.log(data);
 
-  const titleArr = [];
-  const timeFrames = [];
-  for (let i of data) {
-    titleArr.push(i.title);
-    timeFrames.push(i.timeframes);
-  }
-  console.log(timeFrames);
-
-  const btnDay = document.querySelector(".btnDay");
-  const btnWeek = document.querySelector(".btnWeek");
-  const btnMonth = document.querySelector(".btnMonth");
   const titleDiv = document.querySelectorAll(".subtitle");
   const currDiv = document.querySelectorAll(".curr");
   const prevDiv = document.querySelectorAll(".previous");
@@ -35,7 +14,7 @@ async function timeTrackingData() {
       this.classList.add("active");
     });
   });
-  // titleDiv.forEach((item, i) => (item.textContent = titleArr[i]));
+
   const mapa = data.map((el) => el.title);
   titleDiv.forEach((item, i) => {
     item.textContent = mapa[i];
@@ -54,41 +33,9 @@ async function timeTrackingData() {
 
       currDiv.forEach((item, i) => (item.textContent = `${timesCurr[i]}hrs`));
       prevDiv.forEach(
-        (item, i) => (item.textContent = `PREVIOUS - ${timesPrev[i]}hrs`)
+        (item, i) => (item.textContent = `Previous - ${timesPrev[i]}hrs`)
       );
     });
   });
-
-  // btnDay.addEventListener("click", function () {
-  //   const curArr = timeFrames.map((current) => current.daily.current);
-  //   const prevArr = timeFrames.map((current) => current.daily.previous);
-
-  //   currDiv.forEach((item, i) => (item.textContent = `${curArr[i]}hrs`));
-  //   prevDiv.forEach(
-  //     (item, i) => (item.textContent = `PREVIOUS - ${prevArr[i]}hrs`)
-  //   );
-  // });
-  // btnWeek.addEventListener("click", function () {
-  //   const curArr = timeFrames.map((current) => current.weekly.current);
-  //   const prevArr = timeFrames.map((current) => current.weekly.previous);
-
-  //   currDiv.forEach((item, i) => (item.textContent = `${curArr[i]}hrs`));
-  //   prevDiv.forEach(
-  //     (item, i) => (item.textContent = `PREVIOUS - ${prevArr[i]}hrs`)
-  //   );
-  // });
-  // btnMonth.addEventListener("click", function (e) {
-  //   console.log(e.target.textContent);
-  //   // const clickedBtn = e.target.textContent;
-  //   // console.log(clickedBtn.toLowerCase());
-
-  //   const curArr = timeFrames.map((current) => current.monthly.current);
-  //   const prevArr = timeFrames.map((current) => current.monthly.previous);
-
-  //   currDiv.forEach((item, i) => (item.textContent = `${curArr[i]}hrs`));
-  //   prevDiv.forEach(
-  //     (item, i) => (item.textContent = `PREVIOUS - ${prevArr[i]}hrs`)
-  //   );
-  // });
 }
 timeTrackingData();
